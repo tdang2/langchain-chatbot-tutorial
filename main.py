fbd86a87b6f15a4a4ec6286433b9f33934126c4a
Tri Dang <quangtrister@gmail.com>
2025-04-15 18:53:54 -0400

    using langchain to create basic chatbot

--- main.py
+++ main.py
@@ -1,20 +1,67 @@
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import init_chat_model
-from langchain_core.messages import HumanMessage, SystemMessage
+from typing import Annotated, TypedDict
+from IPython.display import Image, display
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_anthropic import ChatAnthropic
+
 
 
 # Load environment variables from .env file
 load_dotenv()
 
+class State(TypedDict):
+     # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+
+
 def main():
-    model = init_chat_model("claude-3-5-haiku-20241022", model_provider="anthropic")
-    messages = [
-        SystemMessage("Translate the following from English into Vietnamese"),
-        HumanMessage("Hello! My name is Miro"),
-    ]
-    result_messages = model.invoke(messages)
-    print(result_messages.pretty_print())
+    def chatbot(state: State):
+        return {
+            "messages": [llm.invoke(state["messages"])]
+        } 
+    
+    graph_builder = StateGraph(State)
+    llm = ChatAnthropic(model="claude-3-5-haiku-20241022")
+    # The first argument is the unique node name
+    # The second argument is the function or object that will be called whenever
+    # the node is used.
+    graph_builder.add_node("chatbot", chatbot)
+    graph_builder.add_edge(START, "chatbot")
+    graph_builder.add_edge("chatbot", END)
+    graph = graph_builder.compile()
+    
+    def stream_graph_updates(user_input: str):
+        for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+            for value in event.values():
+                print("Assistant:", value["messages"][-1].content)
+                
+    try:
+        display(Image(graph.get_graph().draw_mermaid_png()))
+    except Exception:
+        # This requires some extra dependencies and is optional
+        pass
+    
+    while True:
+        try:
+            user_input = input("User: ")
+            if user_input.lower() in ["quit", "exit", "q"]:
+                print("Goodbye!")
+                break
+            stream_graph_updates(user_input)
+        except:
+            # fallback if input() is not available
+            user_input = "What do you know about LangGraph?"
+            print("User: " + user_input)
+            stream_graph_updates(user_input)
+            break
+    
 
 if __name__ == "__main__":
     main()
