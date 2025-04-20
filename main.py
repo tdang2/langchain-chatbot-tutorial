d5f978117064beb466dd0589c28e58b63060d551
Tri Dang <quangtrister@gmail.com>
2025-04-19 20:33:59 -0400

    feat: complete chatbot with tavily search tool

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from langgraph.graph.message import add_messages
 from langchain_anthropic import ChatAnthropic
 from langchain_community.tools.tavily_search import TavilySearchResults
 
+from base_tool_node import BasicToolNode
+
 
 
 # Load environment variables from .env file
@@ -21,6 +23,23 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
+def route_tools(
+    state: State,
+):
+    """
+    Use in the conditional_edge to route to the ToolNode if the last message
+    has tool calls. Otherwise, route to the end.
+    """
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+    return END
+
 
 def main():
     def chatbot(state: State):
@@ -32,13 +51,28 @@ def main():
     llm = ChatAnthropic(model="claude-3-5-haiku-20241022")
     tool = TavilySearchResults(max_results=2)
     tools = [tool]
-    llm_with_tool = llm.bind_tool(tools)
+    llm_with_tool = llm.bind_tools(tools)
     # The first argument is the unique node name
     # The second argument is the function or object that will be called whenever
     # the node is used.
     graph_builder.add_node("chatbot", chatbot)
     graph_builder.add_edge(START, "chatbot")
-    graph_builder.add_edge("chatbot", END)
+    tool_node = BasicToolNode(tools=[tool])
+    graph_builder.add_node("tools", tool_node)
+    # The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "END" if
+    # it is fine directly responding. This conditional routing defines the main agent loop.
+    graph_builder.add_conditional_edges(
+        "chatbot",
+        route_tools,
+        # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+        # It defaults to the identity function, but if you
+        # want to use a node named something else apart from "tools",
+        # You can update the value of the dictionary to something else
+        # e.g., "tools": "my_tools"
+        {"tools": "tools", END: END},
+    )
+    # Any time a tool is called, we return to the chatbot to decide the next step
+    graph_builder.add_edge("tools", "chatbot")
     graph = graph_builder.compile()
     
     def stream_graph_updates(user_input: str):
